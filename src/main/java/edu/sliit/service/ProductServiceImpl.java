a96efe346890a2039c03bdc611b8b923e70c79b2
muditha-edirisingha <it25100028@my.sliit.lk>
2026-04-23 12:03:29 +0530

    Update ProductServiceImpl.java
    updating getProduct

--- src/main/java/edu/sliit/service/ProductServiceImpl.java
+++ src/main/java/edu/sliit/service/ProductServiceImpl.java
@@ -1,19 +1,37 @@
 package edu.sliit.service;
 
 import edu.sliit.dto.Product;
+import edu.sliit.entity.ProductEntity;
+import edu.sliit.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class ProductServiceImpl implements ProductService{
 
+    final ProductRepository repository;
+    final ModelMapper mapper;
+
     @Override
-    public Product getProduct() {
-        return new Product(
-                001,
-                "Jam",
-                "tasty",
-                250.00,
-                100
-        );
+    public  List<Product> getProduct() {
+        List<Product> products = new ArrayList<>();
+        repository.findAll().forEach(product->{
+            products.add(mapper.map(product, Product.class));
+
+        });
+        return products;
+    }
+
+    @Override
+    public void addProduct(Product product) {
+        repository.save(mapper.map(product, ProductEntity.class));
+
+
     }
 }
