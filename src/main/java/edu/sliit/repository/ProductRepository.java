b828798aa5036bdc648579b2872af075759593cf
muditha-edirisingha <it25100028@my.sliit.lk>
2026-04-26 18:05:02 +0530

    Update ProductRepository.java
    adding find-by-product-name and find-by-product-id methods to automatically search

--- src/main/java/edu/sliit/repository/ProductRepository.java
+++ src/main/java/edu/sliit/repository/ProductRepository.java
@@ -3,5 +3,10 @@ package edu.sliit.repository;
 import edu.sliit.entity.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<ProductEntity,Integer> {
+    List<ProductEntity> findByProductName(String productName);
+
+    List<ProductEntity> findByProductId(Integer productId);
 }
