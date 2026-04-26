df287f3ac7fc346ee57841c6fa010038df2824d0
muditha-edirisingha <it25100028@my.sliit.lk>
2026-04-26 18:01:40 +0530

    Update ProductController.java
    adding deleted-by-id and search-by-product-name and search-by-product-id to ProductController

--- src/main/java/edu/sliit/Controller/ProductController.java
+++ src/main/java/edu/sliit/Controller/ProductController.java
@@ -4,6 +4,7 @@ import edu.sliit.dto.Product;
 import edu.sliit.service.ProductService;
 import edu.sliit.service.ProductServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,8 +21,34 @@ public class ProductController {
     }
 
     @PostMapping("/add-product")
+    @ResponseStatus(HttpStatus.CREATED)
     public void addProduct(@RequestBody Product product){
 
         service.addProduct(product);
+    }
+
+    @DeleteMapping("/delete-by-id/{productId}")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public void deleteById(@PathVariable Integer productId){
+        service.deleteById(productId);
+    }
+
+    @PutMapping("/update-product")
+    @ResponseStatus(HttpStatus.OK)
+    public void updateProduct(@RequestBody Product product){
+
+        service.addProduct(product);
+    }
+
+    @GetMapping("/search-by-name/{productName}")
+    public List<Product> searchByName(@PathVariable String productName){
+
+        return service.searchByProductName(productName);
+    }
+
+    @GetMapping("/search-by-productId/{productId}")
+    public List<Product> searchByProductId(@PathVariable Integer productId){
+
+        return service.searchByProductId(productId);
     }
 }
