4d8d6011daf78b429fff330edeae050795aaa867
IT25103266 <IT25103266@my.sliit.lk>
2026-05-04 13:05:06 +0530

    Implemented service layer for supplier

--- src/main/java/edu/sliit/service/SupplierService.java
+++ src/main/java/edu/sliit/service/SupplierService.java
@@ -1,4 +1,17 @@
 package edu.sliit.service;
+import java.util.List;
+
+import edu.sliit.dto.Product;
+import edu.sliit.dto.Supplier;
 
 public interface SupplierService {
+    void addSupplier(Supplier supplier);
+
+    Boolean deletebyId(Integer supplierId);
+
+    List<Supplier> getAll();
+
+    List<Supplier> searchBysupplierName(String supplierName);
+
+    List<Supplier> searchBysupplierId(Integer supplierId);
 }
--- src/main/java/edu/sliit/service/impl/SupplierServiceImpl.java
+++ src/main/java/edu/sliit/service/impl/SupplierServiceImpl.java
@@ -1,5 +1,59 @@
 package edu.sliit.service.impl;
 
-public class SupplierServiceImpl {
+import edu.sliit.dto.Product;
+import edu.sliit.dto.Stock;
+import edu.sliit.dto.Supplier;
+import edu.sliit.entity.SupplierEntity;
+import edu.sliit.repository.SupplierRepository;
+import edu.sliit.service.SupplierService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class SupplierServiceImpl implements SupplierService{
+    final ModelMapper mapper;
+    final SupplierRepository repository;
+    @Override
+    public void addSupplier(Supplier supplier) {
+        repository.save(mapper.map(supplier, SupplierEntity.class));
+    }
+
+    @Override
+    public Boolean deletebyId(Integer supplierId) {
+        repository.deleteById(supplierId);
+        return true;
+    }
+
+    @Override
+    public List<Supplier> getAll(){
+
+        List<Supplier> suppliersList = new ArrayList<>();
+        repository.findAll().forEach(entity ->{
+            suppliersList.add(mapper.map(entity,Supplier.class));
+        });
+        return suppliersList;
+    }
+
+    @Override
+    public List<Supplier> searchBysupplierName(String supplierName) {
+        List<Supplier> suppliers = new ArrayList<>();
+        repository.findBysupplierName(supplierName).forEach(entity ->{
+            suppliers.add(mapper.map(entity,Supplier.class));
+        });
+        return suppliers;
+    }
+
+    @Override
+    public List<Supplier> searchBysupplierId(Integer supplierId) {
+        List<Supplier> suppliers = new ArrayList<>();
+        repository.findBysupplierId(supplierId).forEach(entity ->{
+            suppliers.add(mapper.map(entity,Supplier.class));
+        });
+        return suppliers;
+    }
 }
