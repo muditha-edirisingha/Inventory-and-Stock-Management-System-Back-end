81ed7d3662cd087bb9fb2d54669a840d26fb2433
IT25103266 <IT25103266@my.sliit.lk>
2026-05-04 13:01:30 +0530

    Added suppiler entity

--- src/main/java/edu/sliit/entity/SupplierEntity.java
+++ src/main/java/edu/sliit/entity/SupplierEntity.java
@@ -1,4 +1,26 @@
 package edu.sliit.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
 public class SupplierEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer supplierId;
+    private String supplierName;
+    private String supplierTel;
+    private String email;
+    private String address;
+
 }
