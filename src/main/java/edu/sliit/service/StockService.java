a3cbb918102cd057ef94a878c7d3e5d85761779c
IT25102890 <it25102890@my.sliit.lk>
2026-05-04 16:39:41 +0530

    Update StockService

--- src/main/java/edu/sliit/service/StockService.java
+++ src/main/java/edu/sliit/service/StockService.java
@@ -1,4 +1,16 @@
 package edu.sliit.service;
 
+import edu.sliit.dto.Stock;
+
+import java.util.List;
+
 public interface StockService {
+    void addStock(Stock stock);
+    void reduceStock(Stock stock);
+
+    List<Stock> getAll();
+
+    Stock getStockById(Integer stockId);
+
+    List<Stock> getStockByProductId(Integer productId);
 }
