2cae82421d462f77fb59a93775dd30229d019e1f
IT25103266 <IT25103266@my.sliit.lk>
2026-05-04 11:45:23 +0530

    added GET endpoints

--- src/main/java/edu/sliit/Controller/SupplierController.java
+++ src/main/java/edu/sliit/Controller/SupplierController.java
@@ -1,4 +1,33 @@
 package edu.sliit.Controller;
 
+import edu.sliit.dto.Supplier;
+import edu.sliit.service.SupplierService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/supplier")
+@RequiredArgsConstructor
 public class SupplierController {
+
+    final SupplierService service;
+
+    @GetMapping("/get-all-suppliers")
+    public List<Supplier> getAll(){
+        return service.getAll();
+    }
+
+    @GetMapping("/search-by-name/{supplierName}")
+    public List<Supplier> searchByName(@PathVariable String supplierName){
+
+        return service.searchBysupplierName(supplierName);
+    }
+    @GetMapping("/search-by-id/{supplierId}")
+    public List<Supplier> searchByName(@PathVariable Integer supplierId){
+
+        return service.searchBysupplierId(supplierId);
+    }
 }
