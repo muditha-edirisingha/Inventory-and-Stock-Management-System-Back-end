23b959f9a28bdab0f46990f0f52cf2f33722c889
muditha-edirisingha <it25100028@my.sliit.lk>
2026-05-17 16:28:03 +0530

    Updatre report Controller

--- src/main/java/edu/sliit/Controller/ReportController.java
+++ src/main/java/edu/sliit/Controller/ReportController.java
@@ -1,4 +1,21 @@
 package edu.sliit.Controller;
 
+import edu.sliit.dto.LowStockReport;
+import edu.sliit.service.ReportService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequiredArgsConstructor
+@RequestMapping("/report")
 public class ReportController {
+    final ReportService reportService;
+    @GetMapping("/get-low-stock-report")
+    public List<LowStockReport> getLowStockReport(){
+        return reportService.getLowStockReport();
+    }
+
 }
